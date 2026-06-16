2a5875478aa763e9a75d952ca92ad3b88a2d4cc1
Olalekan Damola <olalekandamolaisaac@gmail.com>
2026-06-16 05:45:43 -0400

    code fixed

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -11,7 +11,10 @@ public class Node
 
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        // Unique values only
+
+        if (value == Data)
+            return;
 
         if (value < Data)
         {
@@ -33,13 +36,34 @@ public class Node
 
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
-    }
+         // Problem 2
 
+        if (value == Data)
+            return true;
+
+        if (value < Data)
+        {
+            if (Left is null)
+                return false;
+
+            return Left.Contains(value);
+        }
+        else
+        {
+            if (Right is null)
+                return false;
+
+            return Right.Contains(value);
+        }
+    }
+    
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // Problem 4
+
+        int leftHeight = Left == null ? 0 : Left.GetHeight();
+        int rightHeight = Right == null ? 0 : Right.GetHeight();
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
